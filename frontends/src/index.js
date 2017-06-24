261e2d0687b66c4178adfb1b556d19e1e22802f6
Tim <tim@rubify.com>
2017-06-24 10:37:35 +0700

    first commit

---
+++ frontends/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+import './App.scss';
+import 'core-js/fn/object/assign';
+import { Router, Route, hashHistory, Redirect } from 'react-router';
+import Layout from './layout/Layout';
+import HomePage from './pages/HomePage';
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Redirect from='/' to='/home' />
+    <Route component={Layout}>
+      <Route path='/home' component={HomePage} />
+    </Route>
+  </Router>
+), document.getElementById('root'));
+registerServiceWorker();

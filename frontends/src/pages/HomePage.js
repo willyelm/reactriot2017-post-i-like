67fc4e0603e0ca0330bcb3772f470a6c86660f3d
Anthony <anthony@rubify.com>
2017-06-24 13:13:58 +0700

    four backend

--- frontends/src/pages/HomePage.js
+++ frontends/src/pages/HomePage.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import HeaderNav from './HeaderNav.js'
+import PostReview from './PostReview.js'
+// import superagent from '../library/Superagent';
+let superagent = require('superagent');
 class HomePage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    this._fetchData()
+  }
+
+  _fetchData() {
+    superagent
+      .get('http://localhost:3000/api/posts/public_posts')
+      .query({
+        token: "8df7154a59394433f558251702beeee0906bd04a"
+      })
+      .end((err, res) => {
+        if (typeof res === 'undefined') {
+          return
+        } else {
+          console.log(res)
+          this.setState({
+            posts: res.body.posts
+          })
+        }
+      })
+  }
+
+  _postList() {
+    let listQuiz = 'Loading data ...'
+    if(this.state.posts.length > 0) {
+      listQuiz = this.state.posts.map((v, k) => {
+        return (
+          <h3>v.title</h3>
+        )
+      })
+
+      return (
+        <div>
+          {listQuiz}
+        </div>
+      )
+    } else {
+      listQuiz = (<div className='text-center'>No data available</div>)
+    }
+    return (
+      <div>
+        {listQuiz}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <HeaderNav />
+          <h2>POST I LIKE</h2>
+          <p>(Uranium team)</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className='listting-posts'>
+          <div className='post-item col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1'>
+            <PostReview />
+          </div>
+        </section>
       </div>
     );
   }
---
+++ frontends/src/pages/PostReview.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+// import superagent from '../library/Superagent';
+let superagent = require('superagent');
+class PostReview extends Component {
+  render() {
+    return (
+      <div className="post-preview">
+          <a href="#">
+              <h2 className="post-title">
+
+              </h2>
+              <h3 className="post-subtitle">
+
+              </h3>
+          </a>
+          <p className="post-meta">Posted by <a href="#"></a> on </p>
+      </div>
+    )
+  }
+}
+
+export default PostReview;

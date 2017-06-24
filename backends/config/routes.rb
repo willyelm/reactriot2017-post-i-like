2018cd65cf3c643e3d889a95d07d3db587876b04
Anthony <anthony@rubify.com>
2017-06-24 11:56:06 +0700

    third backend

--- backends/config/routes.rb
+++ backends/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    # post '/post_token_to_charge', to: 'posts#create'
+    resources :posts do
+      collection do
+        get :public_posts
+      end
+    end
+  end
 end

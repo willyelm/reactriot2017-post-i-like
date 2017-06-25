241f646e063aa04b56e0268940c0a9f243fe13ee
Orange Vo <orange@rubify.com>
2017-06-26 00:07:17 +0700

    update

--- frontends/src/library/Superagent.js
+++ frontends/src/library/Superagent.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV === 'development') {
   }
 } else {
   window.config = {
-    apiHost: 'http://post-i-like.projectwebby.com'
+    apiHost: 'http://post-i-like.projectwebby.com:8080'
   }
 }
 

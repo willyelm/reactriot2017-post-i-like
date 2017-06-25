d8f6381fc22cd0730836a03f253459ffd33e3f8f
Anthony <anthony@rubify.com>
2017-06-25 08:57:16 +0700

    delete postreview

--- frontends/src/pages/HomePage.js
+++ frontends/src/pages/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import HeaderNav from './HeaderNav.js'
-import PostReview from './PostReview.js'
 import superagent from '../library/Superagent';
 class HomePage extends Component {
 
